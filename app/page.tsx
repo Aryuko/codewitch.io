7ed586611cfdb2a833a5011b524b1c27cbdfe5cc
Aryuko <pachimaryu@gmail.com>
2024-09-17 19:54:31 +0200

    Add contact links

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 "use client"
+import Link from "next/link"
 import React from "react"
 
 export default function Home() {
     return (
         // Container
-        <div className="flex-center h-dvh w-dvw flex-col gap-4 text-center font-light leading-relaxed">
+        <div className="flex h-dvh w-dvw flex-col items-center gap-8 text-center font-light leading-relaxed">
             {/* TODO: Make sure title stays centered-ish even with more content below */}
             {/* Title */}
-            <h1 className="animate-title mt-0 text-6xl font-light tracking-normal md:text-8xl lg:text-9xl">
+            <h1 className="animate-title mt-[35dvh] text-6xl font-light tracking-normal md:text-8xl lg:text-9xl">
                 codewitch
             </h1>
             {/* Description */}
@@ -19,7 +20,17 @@ export default function Home() {
                 <p>Engineer, entrepreneur, cat mother.</p>
             </div>
             {/* Links and socials */}
-            <div></div>
+            <div className="*:text-discreet animate-fade-in flex gap-6 text-4xl *:transition-all hover:*:text-base-content md:text-4xl">
+                <Link target="_blank" href="https://github.com/aryuko">
+                    <i className="fa-brands fa-github" />
+                </Link>
+                <Link target="_blank" href="https://x.com/_codewitch">
+                    <i className="fa-brands fa-twitter" />
+                </Link>
+                <Link href="mailto:hanna@codewitch.io">
+                    <i className="fa-solid fa-envelope" />
+                </Link>
+            </div>
             {/* Projects */}
             <div></div>
         </div>
